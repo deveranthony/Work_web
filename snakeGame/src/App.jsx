30ef08baf554da511a4b5241ab443111609aa9a6
deveranthony <deveranthony5@gmail.com>
2025-09-16 00:22:37 -1100

    change

--- snakeGame/src/App.jsx
+++ snakeGame/src/App.jsx
@@ -1,17 +1,56 @@
-import { useState } from 'react'
 import './App.css'
 
+function Canvas() {
+  return(
+    <>
+      <h1>Snake</h1>
+        <div className='gamearea'>
+          <canvas id='gameCanvase' width={400} height={400} aria-label='Snake Game'></canvas>
+        </div>
+    </>    
+  )
+}
+
+function Asside(){
+  return(
+    <>
+      <aside className='hud'>
+        <div className='scoreboard'>
+          <div>Score: <span id='score'>0</span></div>
+          <div>High: <span id='highScore'>0</span></div>
+        </div>
+
+        <div className='controls'>
+          <button id='startBtn'>Start</button>
+          <button id='pauseBtn'>Pause</button>
+          <button id='restartBtn'>Restart</button>
+        </div>
+
+        <div className='hint'>
+          Controls: Arrow keys or WASD
+        </div>
+
+        <div className='mobile-controls' id='mobileControls' aria-hidden='true'>
+          <button data-dir='up'>↑</button>
+          <div className='h-row'>
+            <button data-dir='left'>←</button>
+            <button data-dir='down'>↓</button>
+            <button data-dir='right'>→</button>
+          </div>
+        </div>
+
+      </aside>
+    </>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <main className='container'>
+        <Canvas/>
+        <Asside/>
+      </main>
     </>
   )
 }
